47858f9f06d97d716240b79a86c74953a1a8cfdf
vetri-selvam <114853173+vetri-selvam@users.noreply.github.com>
2024-11-20 23:12:55 +0530

    Hobbit code

--- Head-First-Java-Codes/HobbitTestDrive.java
+++ Head-First-Java-Codes/HobbitTestDrive.java
@@ -1,13 +1,15 @@
+// Chapter 2, Page 63
+
 class Hobbits {
     String name;
 }
 
-public class roughPaper {
+public class HobbitTestDrive {
     public static void main(String[] args) {
         Hobbits[] hob = new Hobbits[4];
-        int z = 0;
+        int z = 0; // or z = -1
 
-        while (z > 3) {
+        while (z < 3) { // or (z < 2)
             hob[z] = new Hobbits();
             hob[z].name = "Bilbo";
             if (z == 1) {
@@ -16,7 +18,7 @@ public class roughPaper {
             if (z == 2) {
                 hob[z].name = "Sam";
             }
-            System.out.println(hob[z].name + "is a good hobbit");
+            System.out.println(hob[z].name + " is a good hobbit");
             z = z + 1;
         }
     }
