95ace3a78dfd76e436e091714c2901be1adec4c8
Vetri Selvam <vetriwork1@gmail.com>
2025-01-23 11:56:12 +0530

    FindDuplicate method 2

--- Beginner Coding/FindDuplicate.java
+++ Beginner Coding/FindDuplicate.java
@@ -3,6 +3,8 @@ import java.util.Set;
 
 public class FindDuplicate {
 
+    // METHOD 1 (Efficient)
+
     static Set<Integer> findDuplicate ( int[] arr){
 
         // store duplicate elements in a HashSet
@@ -17,16 +19,44 @@ public class FindDuplicate {
          * When fails, add that element into the
          * duplicateArray.
          */
-        for(int i=0; i<arr.length; i++){
-            if (!set.add(arr[i])){
-                duplicateArray.add(arr[i]);
+        for(int element : arr){
+            if (!set.add(element)){
+                duplicateArray.add(element);
             }
         }
+        System.out.println(set);
         return duplicateArray;
     }
 
+    // METHOD 2 (If an array must be retured)
+    
+    /**
+     * static Set<Integer> findDuplicates(int[] arr) {
+            //To store the duplicate elements in the array use this,
+            // int j = 0;
+            // int[] duplicateArray = new int[arr.length];
+
+            Set<Integer> set = new HashSet<>();
+
+            //To avoid trailing 0's and same duplicate elements added to the array use HashSet to store it
+            Set<Integer> duplicateElements = new HashSet<>();
+            for (int num : arr) {
+                if (!set.add(num)) {
+                    duplicateArray[j]=num;
+                    // j++;  //for array
+
+                }
+            }
+            // return Arrays.copyOfRange(duplicateArray, 0, j); //To trim the trailing 0's in the duplicate elements array
+            return duplicateElements;
+        }
+     * 
+     */
+
+
     public static void main(String[] args) {
-       int[] arr = {1,2,3,3,3,4,8,10,10};
+       int[] arr = {1,2,3,3,3,4,8,10,10, 4, 0,0};
        System.out.println(findDuplicate(arr));
     }
 }
+
